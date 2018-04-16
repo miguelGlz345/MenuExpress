8f0b22d72324b3fabda62f34830a9211f267a265
Miguel Gonzalez Pedraza <migpxel34.5@gmail.com>
2018-04-16 02:44:47 -0500

    welcome

---
+++ app/src/main/java/com/menuexpress/equipo6/menuexpress/WelcomeActivity.java
@@ -0,0 +1,25 @@
+package com.menuexpress.equipo6.menuexpress;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    LinearLayout l1, l2;
+    Animation uptodown, downtoup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        l1 = (LinearLayout) findViewById(R.id.line1);
+        l2 = (LinearLayout) findViewById(R.id.line2);
+        uptodown = AnimationUtils.loadAnimation(this, R.anim.uptodown);
+        downtoup = AnimationUtils.loadAnimation(this, R.anim.downtoup);
+        l1.setAnimation(uptodown);
+        l2.setAnimation(downtoup);
+    }
+}
